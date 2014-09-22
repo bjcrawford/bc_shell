d8342c6c440e071d762f71e9098e79c05d94691e
Brett Crawford <brettjamescrawford@gmail.com>
2014-09-22 15:20:09 -0400

    Added ability to check environment paths for command name, added function for parsing paths, added function for handling path parsing errors

--- bc_shell.c
+++ bc_shell.c
@@ -19,61 +19,109 @@
 /* Error codes */
 #define EXIT_SUCCESS 0
 #define ALLOC_ERROR 1
+#define PATH_PARSE_ERROR 2
+
+/* External variables */
+extern char **environ;
 
 /* Function declarations */
-int shellPrompt();
-int parseInput(int*, char***);
+int shell_prompt();
+int parse_path(char***);
+int parse_input(int*, char***);
 int allocation_error(char*);
-int chopTest();
+int path_parse_error();
 
 int main(int argc, char **argv)
 {
-	return shellPrompt();
+	return shell_prompt();
 }
 
-int shellPrompt()
+int shell_prompt()
 {
 	int i;
 	int i_argc;
+	int found;
+	char *temp;
+	char *command;
+	char **paths;
 	char **i_argv;
 	char **dp;
 
-	while(parseInput(&i_argc, &i_argv))
-	{
-		/*
-		printf("i_argc: %d\n", i_argc);
-		dp = i_argv;
-		while(*dp != NULL)
-		{
-			printf("argv[%d]: %s\n", dp - i_argv, *dp);
-			dp++;
-		}
-		*/
+	if(!parse_path(&paths))
+		return path_parse_error();
 
+	printf("Paths found in $PATH:\n");
+	for(i = 0; paths[i] != NULL; i++)
+		printf("paths[%d]: %s\n", i, paths[i]);
+
+	while(parse_input(&i_argc, &i_argv))
+	{
+		found = 0;
 		if(access(i_argv[0], F_OK) != -1)
 		{
-			// Command exists
-			printf("Running command %s\n", i_argv[0]);
+			command = i_argv[0];
+			found = 1;
 		}
 		else
 		{
-			// Command not found
-			printf("-bc_shell: %s: command not found\n", i_argv[0]);
+			for(i = 0; paths[i] != NULL; i++)
+			{
+				temp = append("/", i_argv[0]);
+				command = append(paths[i], temp);
+				printf("Checking: %s\n", command);
+				if(access(command, F_OK) != -1)
+				{
+					found = 1;
+					break;
+				}
+			}
+			
 		}
+		if(found)
+		{
+			printf("Found command: %s located at %s\n", i_argv[0], command);
+		}
+		else
+			printf("-bc_shell: %s: command not found\n", i_argv[0]);
 		
+		free(temp);
+		free(command);
 		for(i = 0; i_argv[i] != NULL; i++)
 			free(i_argv[i]);
 		free(i_argv);
 	}
 
-	
+	free(temp);
+	free(command);
+	for(i = 0; i_argv[i] != NULL; i++)
+		free(i_argv[i]);
+	free(i_argv);
+	free(paths);
 
 	printf("Exiting...\n");
 
 	return EXIT_SUCCESS;
 }
 
-int parseInput(int *i_argc, char ***i_argv)
+int parse_path(char ***paths)
+{
+	int i = 0;
+	char *path;
+
+	while(environ[i] != NULL && !begins_with_ignore_case(environ[i], "path="))
+		i++;
+	path = replace(environ[i], "PATH=", "");
+	*paths = chop(path, ':');
+
+	free(path);
+
+	if(*paths == NULL)
+		return 0;
+	else
+		return 1;
+}
+
+int parse_input(int *i_argc, char ***i_argv)
 {
 	int i;
 	int c;        /* Input character storage */
@@ -111,18 +159,13 @@ int allocation_error(char *name)
 	return ALLOC_ERROR;
 }
 
-int chopTest()
+/* Reports a path parsing error
+   Output: integer, error code for allocation error */
+int path_parse_error()
 {
-	printf("\n\n====== chop() tests start ======\n\n");
-	char s47[] = "I am ready for a nice vacation.";
-	char **words = chop(s47, ' ');
-	int i = 0;
-	printf("%d: %s\n", i, words[i]);
-	do {
-		i++;
-		printf("%d: %s\n", i, words[i]);
-	} while(words[i] != NULL);
-	free(words);
+	fprintf(stderr, "Error parse the path environment variable\n");
+	fprintf(stderr, "Press enter to exit.\n");
+	getchar();
 
-	return EXIT_SUCCESS;
+	return PATH_PARSE_ERROR;
 }
