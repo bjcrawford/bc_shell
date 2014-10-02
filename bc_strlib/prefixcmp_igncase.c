987f2731035f8fee4c5cdfc6d17af28436a3f6fd
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-02 15:50:33 -0400

    Modified prefixcmp_igncase to return false is either string is blank

--- bc_strlib/prefixcmp_igncase.c
+++ bc_strlib/prefixcmp_igncase.c
@@ -5,9 +5,12 @@
 
 #include "bc_strlib.h"
 
-/* Returns 1 if pre is a prefix of s, ignoring case. Otherwise 0 */
+/* Returns 1 if pre is a prefix of s, ignoring case. Otherwise 0 
+   If either s or pre is blank, 0 is returned */
 int prefixcmp_igncase(char *s, char *pre)
 {
+	if(is_blank(s) || is_blank(pre))
+		return 0;
 	int sl = strlen(s);
 	int pl = strlen(pre);
 	while(sl-- && pl--)
